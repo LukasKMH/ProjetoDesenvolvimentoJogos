082f96e8774fb8301bdf4050e374ef8925d874db
Sauter <gabriel.andrade.sauter@gmail.com>
2023-11-29 12:21:56 -0300

    Added initial logic of the npc mage

---
+++ NPCMage/mage.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+var interactable = false
+
+func _ready():
+	respawn()
+	self.visible = false
+
+func _process(delta):
+	if !interactable:
+		return
+	
+	if Input.is_action_just_pressed("interact"):
+		print("e pressed")
+
+func respawn():
+	var rand = RandomNumberGenerator.new()
+	rand.randomize()
+	var x = rand.randf_range(32, 704)
+	rand.randomize()
+	var y = rand.randf_range(40, 416)
+	
+	var player = get_parent().find_child("Player")
+	while player.global_position.distance_to(Vector2(x, y)) < 100.0:
+		rand.randomize()
+		x = rand.randf_range(32, 704)
+		rand.randomize()
+		y = rand.randf_range(40, 416)
+	
+	# TODO mago não deve dar respawn numa parede
+	
+	self.position.x = x
+	self.position.y = y
+
+func _on_interact_area_body_entered(body):
+	if body is Player:
+		interactable = true
+		self.visible = true
+
+func _on_interact_area_body_exited(body):
+	if body is Player:
+		interactable = false
+		self.visible = false
